470babc4f04b5bbc9778e088080aecf619215b34
Mohamed Ibrahim <mohamed.ibraham98@gmail.com>
2025-10-19 15:20:16 +0300

    add timer and countdown

--- main.py
+++ main.py
@@ -16,13 +16,81 @@ periods = {}
 periods_num = 4
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    global  current_duration, timer_is_on
+    timer_is_on = False
+    current_duration = 0
+    start_button.config(text="start")
+    timer_label.config(text=f"Idle")
+    canvas.itemconfig(timer_id, text=f"{format_time()}")
+    periods.clear()
 
+def pause_handler():
+    global timer_is_on
+    if timer_is_on:
+        timer_is_on = False
+        start_button.config(text="start")
+    else:
+        start_button.config(text="Pause")
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global periods, timer_is_on, current_duration, periods_num
 
+    if timer_is_on:
+        pause_handler()
+        return
+
+    if current_duration < 0.1:
+        for n in range(periods_num+1):
+            if n < periods_num:
+                periods[f"Work Session: {n+1}"] = WORK_MIN
+                periods[f"Short Break: {n+1}"] = SHORT_BREAK_MIN
+            else:
+                periods["Break"] = LONG_BREAK_MIN
+    else:
+        pause_handler()
+        timer_is_on = True
+
+    start_countdown()
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def start_countdown():
+    global  current_duration, timer_is_on, canvas
 
+    if current_duration < 0.1:
+        key_list = list(periods.keys())
+        value_list = list(periods.values())
+        current_duration = value_list[0] * 60 + 1
+        timer_label.config(text=f"{key_list[0]}")
+        del periods[key_list[0]]
+        pause_handler()
+        timer_is_on = True
+        window.deiconify()
+        window.wm_attributes("-topmost", False)
+
+    while timer_is_on:
+        global check_mark, periods_num
+
+        if current_duration >= 0.1:
+            current_duration -= 0.1
+
+        if current_duration < 0.1 and len(periods) > 0:
+            timer_is_on = False
+            window.wm_attributes("-topmost", True)
+            window.update()
+            start_countdown()
+        elif current_duration < 0.1 and len(periods) == 0:
+            reset_timer()
+
+        canvas.update()
+        canvas.itemconfig(timer_id, text=f"{format_time()}")
+
+        time.sleep(0.1)
+
+def format_time():
+    time_struct = time.gmtime(current_duration)
+    return time.strftime("%M:%S", time_struct)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
